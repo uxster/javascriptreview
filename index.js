a64994d7c1ff885646a0ef36329eec118957b5e6
uxster <hello@uxster.nl>
2017-07-10 11:09:41 +0200

    made changes that enables both jest-testing and running the script normally with node

--- index.js
+++ index.js
@@ -7,33 +7,34 @@ function sum(a, b, c) {
 	return result;
 };
 
-sum(1, 2, 3);
-sum(8998, 1, 1);
+sum(1, 2, 3);			//6
+sum(8998, 1, 1);		//9000
+
+logger("/////////////////////////");
 
 // given a string, create a function that returns the last character in that string.
-// examples:
-// "cattywampus" --> s
 function lastChar(string) {
 	var result = string[string.length - 1];
 	logger(result);
 	return result;
 };
 
-lastChar('cattywampus');
-lastChar('Thats amazing!');
+lastChar('cattywampus');		//s
+lastChar('Thats amazing!');		//!
+
+logger("/////////////////////////");
 
 // write a function that takes in one parameter and returns the cube of that parameter
-// examples:
-// 3 --> 27
-// 4 --> 64
 function cube(number) {
 	var result = number * number * number;
 	logger(result);
 	return result;
 };
 
-cube(3);
-cube(4);
+cube(3);		//27
+cube(4);		//64
+
+logger("/////////////////////////");
 
 // define a function that takes in a string and reverses it. you are not allowed to
 // call the "reverse" function (or any other string functions)
@@ -55,15 +56,14 @@ function reversed(string) {
 	return emptyString;
 }
 
-reversed('Hello');
-reversed('Hello this is dog!');
+reversed('Hello');					//olleH
+reversed('Hello this is dog!');		//!god si siht olleH
+
+logger("/////////////////////////");
 
 // write a function that takes in two arrays of the same length as parameters. From those two arrays,
 // create, then return an object which contains the elements of the first array as keys, and the
 // elements of the second array as values.
-// examples:
-// ["exciting", "exotic"], ["markets", "britain"] --> { exciting: "markets", exotic: "britain" }
-// ["a", "b", "c"], ["a", "b", "c"] --> { a: "x", b: "y", c: "z" }
 var array1 = ["exciting", "exotic"];
 var array2 = ["markets", "britain"];
 var array3 = ["a", "b", "c"];
@@ -71,7 +71,6 @@ var array4 = [3, true, 42];
 
 function filler(arr1, arr2) {
 	var emptyObject = {};
-	// emptyObject[arr1[0]] = arr2[0];
 
 	for (var i = 0; i < arr1.length; i++) {
 		emptyObject[arr1[i]] = arr2[i];
@@ -81,17 +80,16 @@ function filler(arr1, arr2) {
 	return emptyObject;
 }
 
-filler(array1, array2);
-filler(array3, array4);
+filler(array1, array2);		//{ exciting: 'markets', exotic: 'britain' }
+filler(array3, array4);		//{ a: 3, b: true, c: 42 }
+
+logger("/////////////////////////");
 
 // Given an object with keys and values, create two arrays: one which contains the object's keys,
 // and one which contains the object's values. Wrap this into a function which takes in one object
 // that contains keys and values, and returns a different object that contains two keys. The first key
 // should be named "keys" and will have the first array as a value. The second key should be named
 // "values" and will have the second array as a value.
-// examples:
-// { exciting: "markets", exotic: "britain" } --> { keys: ["exciting", "exotic"], values: ["markets", "britain"] }
-// { a: "x", b: "y", c: "z" } --> { keys: ["a", "b", "c"], values: ["x", "y", "z"] }
 var obj1 = { exciting: "markets", exotic: "britain" };
 var obj2 = { a: "x", b: "y", c: "z" };
 
@@ -112,8 +110,10 @@ function rellif(obj) {
 	return result;
 }
 
-rellif(obj1);					//expected: { keys: ["exciting", "exotic"], values: ["markets", "britain"] }
-rellif(obj2);					//expected: { keys: ["a", "b", "c"], values: ["x", "y", "z"] }
+rellif(obj1);		//{ keys: ["exciting", "exotic"], values: ["markets", "britain"] }
+rellif(obj2);		//{ keys: ["a", "b", "c"], values: ["x", "y", "z"] }
+
+logger("/////////////////////////");
 
 // OPTIONAL CHALLENGE 1
 // write a function that takes in two parameters, x and n, and computes x to the n'th power
@@ -127,21 +127,21 @@ function pow(x, n) {
 	return result;
 }
 
+pow(8, 2);		//64
+pow(4, 9);		//262144
+pow(4, 6);		//4096
+
+logger("/////////////////////////");
+
 // OPTIONAL CHALLENGE 2
 // Jon has terrible social anxiety, and wishes to sit as far away from everyone as possible.
 // Given an array of booleans, where the array represents a row of seated people, and each
 // boolean represents whether the seat is occupied or not, find the ideal seat for Jon.
 
 var people = [true, false, true, true, true, true, true, false, false, false, false, false, true];
-
 //expected value: i = 9
 
-
-
-
-
-
-
+//export all the function to be able to test them with jest
 module.exports = {
 	sum,
 	lastChar,
